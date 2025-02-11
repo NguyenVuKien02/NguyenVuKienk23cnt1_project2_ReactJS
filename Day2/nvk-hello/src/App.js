c34eb55e79998d7fdbde1d0d8692b281ee3d41cb
nvk-lesson4 <nguyenvukien02@gmail.com>
2025-02-11 09:15:23 +0700

    Day

---
+++ Day2/nvk-hello/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Thông tin cá nhân</h1>
+        <p>
+          <strong>Họ và tên:</strong> Nguyễn Vũ Kiên
+        </p>
+        <p>
+          <strong>Mã sinh viên:</strong> 2310900053
+        </p>
+        <p>
+          <strong>Ngày sinh:</strong> 02/10/2005
+        </p>
+        <p>
+          <strong>Điện thoại:</strong> 0379661853
+        </p>
+        <p>
+          <strong>Tên lớp:</strong> Lớp K23CNT1 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
